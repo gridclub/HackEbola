6adb450e3c5a5114f3b6dd46edd7c02423590e51
Stephen A Lauer <stephenalauer@gmail.com>
2014-11-23 13:44:43 -0500

    saved ebola-donations-final.csv

--- project/ebola-donation/global.R
+++ project/ebola-donation/global.R
@@ -17,33 +17,6 @@ pkgTest("tidyr")
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", 
                 "#0072B2", "#D55E00", "#CC79A7")
 
-cases_interp <- read.csv("~/Google Drive/HackEbola/project/ebola-donation/cases_interp.csv")[,-1]
-cleaned_donations <- read.csv("~/Google Drive/HackEbola/project/ebola-donation/cleaned_donations.csv")[,-1]
+ebola_dat <- read.csv("~/Google Drive/HackEbola/project/ebola-donation/ebola-donations-final.csv")
 
-cases_interp$Date <- ymd(cases_interp$Date)
-cleaned_donations$Date <- ymd(cleaned_donations$Date)
-cases_interp$country <- as.character(cases_interp$country)
-cleaned_donations$destination <- as.character(cleaned_donations$destination)
-
-cleaner_donations <- cleaned_donations %>%
-  group_by(Date, destination) %>%
-  summarise(USD_given = sum(USD.given))
-
-dates <- seq.Date(from = as.Date("2014-01-01"), to = as.Date("2014-11-20"), by = 1)
-donations_interp <- data.frame(Date = rep(dates, each = 3),
-                               country = rep(c("Guinea", "Sierra Leone", "Liberia"), 
-                                             length(dates)))
-donations_interp2 <- merge(x = donations_interp, y = cleaner_donations, 
-                           by.x = c("Date", "country"), by.y = c("Date", "destination"), 
-                           all.x = TRUE)  
-
-donations_interp2$USD_given <- ifelse(is.na(donations_interp2$USD_given), 0, 
-                                      donations_interp2$USD_given)
-
-donations_interp3 <- donations_interp2 %>%
-  group_by(country) %>%
-  mutate(USD_interp = cumsum(USD_given))
-
-ebola_dat <- merge(x=cases_interp, y=donations_interp3, all=TRUE)
-
-ebola_dat$Date <- as.Date(ebola_dat$Date)
+ebola_dat$Date <- as.Date(ymd(ebola_dat$Date))
